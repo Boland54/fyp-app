fe03709bc7eeaeb031945c7e6a1a7932118f53bf
Adam Boland <20084498@mail.wit.ie>
2022-02-02 09:52:59 +0000

    WIP2

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,9 +1,29 @@
 import React from "react";
 
+
+
 export const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <table>
+        <thead>
+<tr>
+  <th>Name</th>
+  <th>Address</th>
+  <th>Phone Number</th>
+  <th>Email</th>
+</tr>
+        </thead>
+<tbody>
+  <tr>
+    <td>Adam Boland</td>
+    <td>Mitchelstown</td>
+    <td>0873930673</td>
+    <td>Adamboland123@gmail.com</td>
+  </tr>
+</tbody>
+
+        </table>
     </div>
   );
 };
